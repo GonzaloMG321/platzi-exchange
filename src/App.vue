6a059c0861794f65e1a6cf220f1885634dc6a557
Gonzalo <gonzalo.mendoza.itp@gmail.com>
2019-12-02 23:13:15 -0600

    Agregando vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <main>
-  
+    <px-header />
+    <px-assets-table />
   </main>
 </template>
 
 <script>
-
+import PxHeader from '@/components/PxHeader'
+import PxAssetsTable from '@/components/PxAssetsTable'
 export default {
   name: "app",
   components: {
-    
+    PxHeader,
+    PxAssetsTable
   }
 };
 </script>
